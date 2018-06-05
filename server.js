c41572f0292e571fbb159444715b04c889697846
Viktor <npibet2013@gmail.com>
2018-06-05 13:09:51 +0300

    auth

--- server.js
+++ server.js
@@ -8,21 +8,9 @@ const router = express.Router();
 const port = process.env.PORT || 5000;
 const compression = require("compression");
 
-function shouldCompress(req, res) {
-  if (req.headers["x-no-compression"]) return false;
-  return compression.filter(req, res);
-}
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(compression({
-  level: 9,               // set compression level from 1 to 9 (6 by default)
-  filter: shouldCompress, // set predicate to determine whether to compress
-}));
-
-app.get('*bundle.js', function (req, res, next) {
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
-  next();
-});
+app.use('/', expressStaticGzip(path.join(__dirname, 'dist'), {
+  enableBrotli: true
+}))
 
 app.use(session({secret: 'abc'}));
 
